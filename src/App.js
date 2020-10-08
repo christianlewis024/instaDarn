f31a05844c056a612e29b6834310ff380a4b04ba
Christian Lewis <christianlewis024@gmail.com>
2020-10-08 16:39:41 -0700

    setup file and import of the imageUpload.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { db, auth } from "./firebase";
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import { Button, Input } from "@material-ui/core";
+import ImageUpload from "./ImageUpload";
 
 function getModalStyle() {
   const top = 50;
@@ -97,6 +98,7 @@ function App() {
   };
   return (
     <div className="app">
+      <ImageUpload />
       <Modal open={open} onClose={() => setOpen(false)}>
         <div style={modalStyle} className={classes.paper}>
           <form className="app_signup">
